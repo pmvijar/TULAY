c1f6d7f44f1f61116232f2c19e2dfc76062343b2
erratical <105215848+erratical@users.noreply.github.com>
2025-01-16 02:13:32 +0800

    chore: fix

--- backend/Routers/gis.routes.js
+++ backend/Routers/gis.routes.js
@@ -6,13 +6,17 @@ import {
   uploadGeoJSON,
   uploadPedestrianGeoJSON,
   uploadStationGeoJSON,
+  uploadWays,
 } from "../Controllers/gis.controller.js";
 
 async function GISRoutes(fastify) {
   fastify.post("/upload-geojson", uploadGeoJSON);
   fastify.post("/upload-station", uploadStationGeoJSON);
   fastify.post("/upload-passage", uploadPedestrianGeoJSON);
+  fastify.post("/upload-ways", uploadWays);
+
   fastify.post("/calc-proximity", calculateProximityScore);
+
   fastify.post("/get-geounits", getGeoUnits);
   fastify.post("/get-stations", getStations);
   fastify.post("/get-passages", getPedestrianPassages);
